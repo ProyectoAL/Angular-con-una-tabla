9c8c1540911d1695b5aa7aa5347efa3a429f5fab
XavierC14 <74736448+XavierC14@users.noreply.github.com>
2023-02-24 16:25:11 +0100

    Add files via upload

--- src/app/paginas/perfil/perfil.component.ts
+++ src/app/paginas/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Alumnos } from 'src/app/models/alumnos.model';
+import { Profesores } from 'src/app/models/profesores.model';
 import { UsuariosService } from 'src/app/servicio/usuarios.service';
 
 @Component({
@@ -8,10 +10,31 @@ import { UsuariosService } from 'src/app/servicio/usuarios.service';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor(private usuarios: UsuariosService) { }
+  constructor(public usuarios: UsuariosService) { }
+
+  alumnos: Alumnos[] = [];
+  profesores: Profesores[] = [];
 
   ngOnInit(): void {
     console.log(this.usuarios.datosusuario);
   }
 
-}
+  getAlumno(): void {
+    this.usuarios.getAlumnos().subscribe({
+      next: (value: Alumnos[]) => {
+        this.alumnos = [];
+        this.alumnos = value;
+      }
+    })
+  }
+
+  getProfesor(): void {
+    this.usuarios.getProfesores().subscribe({
+      next: (value: Profesores[]) => {
+        this.profesores = [];
+        this.profesores = value;
+      }
+    })
+  }
+
+} 
--- src/app/servicio/usuarios.service.ts
+++ src/app/servicio/usuarios.service.ts
@@ -1,3 +1,86 @@
+<<<<<<< HEAD
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { filter } from 'rxjs/operators';
+import { Login } from '../models/login.model';
+import { Profesores } from '../models/profesores.model';
+import { Alumnos } from '../models/alumnos.model';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UsuariosService {
+
+    readonly URL = "http://127.0.0.1:8000/api/";
+
+    constructor(private _http: HttpClient) { }
+
+    datosusuario: any;
+
+    addAlumnos(alumno: Alumnos) {
+        return this._http.post(this.URL + "signup", alumno)
+            .pipe(
+                filter((response: any) => {
+                    let found = false;
+                    if (response != null) {
+                        found = true;
+                    }
+                    else {
+                        found = false;
+                    }
+                    this.datosusuario = response;
+                    return found;
+                }
+                ));
+    }
+
+    addProfesores(profesor: Profesores) {
+        return this._http.post(this.URL + "signup", profesor)
+            .pipe(
+                filter((response: any) => {
+                    let found = false;
+                    if (response != null) {
+                        found = true;
+                    }
+                    else {
+                        found = false;
+                    }
+                    this.datosusuario = response;
+                    return found;
+                }
+                ));
+    }
+
+    login(login: Login) {
+        return this._http.post(this.URL + "login", login)
+            .pipe(
+                filter((response: any) => {
+                    let found = false;
+                    if (response != null) {
+                        found = true;
+                    }
+                    else {
+                        found = false;
+                    }
+                    this.datosusuario = response;
+                    return found;
+                }
+                ));
+    }
+
+    getAlumnos() {
+        let url: string = this.URL;
+        return this._http.get<Alumnos[]>(url + "");
+    }
+
+    getProfesores() {
+        let url: string = this.URL
+        return this._http.get<Profesores[]>(url);
+    }
+
+=======
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { filter } from 'rxjs/operators';
@@ -68,4 +151,5 @@ export class UsuariosService {
                 ));
     }
 
+>>>>>>> 60180c695402d4b407e41b91d35af83182a7bc31
 }
